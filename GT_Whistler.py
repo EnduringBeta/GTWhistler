0578bf0e3ea93fb8cf806ed452919ad2d5c1c4d1
Boss1000 <boss1001@gmail.com>
2016-04-02 01:17:48 -0400

    Added schedule JSON file and simplified code.

--- GT_Whistler.py
+++ GT_Whistler.py
@@ -15,72 +15,99 @@ import json
 
 from datetime import datetime
 from pytz import timezone
-from time import sleep
+from time import sleep, strftime
 
 class Whistler:
     """Class that holds functionality for processing conditions to tweet as @GT_Whistle twitter account"""
 
-    tz = timezone('US/Eastern')
-    dt = datetime
-    whistled = False;
+    tz          = timezone('US/Eastern')
+    dt          = datetime
+    dtFormat    = "%a, %d %b %Y - %H:%M:%S"
+    whistled    = False
 
-    Monday = 0
-    Friday = 5
-    morningHour = 7
-    eveningHour = 12 + 5
-    classStartMinute = 5
-    classEndMinute = 55
+    Monday      = 0
+    Tuesday     = 1
+    Wednesday   = 2
+    Thursday    = 3
+    Friday      = 4
+    Saturday    = 5
+    Sunday      = 6
 
-    def setup(self):
-        # Custom file for authentication data not to be shared publicly.
-        configFile = "myConfig.json"
-
+    def twitterSetup(self, twitterConfigFile):
         try:
-            with open(configFile, encoding='utf-8') as dataFile:
-                config = json.loads(dataFile.read())
+            with open(twitterConfigFile, encoding='utf-8') as dataFile:
+                self.twitterConfig = json.loads(dataFile.read())
         except Exception as e:
             print("Error when loading configuration:", e)
 
         try:
             t = Twitter(auth=OAuth(
-                consumer_key    =   config["consumer_key"],
-                consumer_secret =   config["consumer_secret"],
-                token           =   config["access_token"],
-                token_secret    =   config["access_token_secret"]))
+                consumer_key    =   twitterConfig["consumer_key"],
+                consumer_secret =   twitterConfig["consumer_secret"],
+                token           =   twitterConfig["access_token"],
+                token_secret    =   twitterConfig["access_token_secret"]))
         except Exception as e:
             print("Error when authenticating with Twitter:", e)
 
-    def whistle(self):
-        print("Tweet\r\n")
+    def scheduleSetup(self, scheduleConfigFile):
+        try:
+            with open(scheduleConfigFile, encoding='utf-8') as dataFile:
+                self.scheduleConfig = json.loads(dataFile.read())
+        except Exception as e:
+            print("Error when loading schedule:", e)
+
+    def specialSoundSetup(self):
+        # Set up "When The Whistle Blows" time and football information
+        # (http://www.specialevents.gatech.edu/our-events/when-whistle-blows)
+        return
+
+    def whistleTweet(self):
+        t.statuses.update(status="Woo")
+
+    def whistlePrint(self):
+        print("WOOOO @ {0}\r\n".format(dt.strftime(dtFormat)))
+
+    def scheduleWhistle(self):
+        #self.whistleTweet()
+        self.whistlePrint()
+
         self.whistled = True
+        sleep(15 * 60) # Do nothing for X seconds/minutes
+
+    def wtwbWhistle(self):
+        return
+
+    def footballWhistle(self):
+        return
 
     def start(self):
+
+        schedule = self.scheduleConfig['schedule']
+
         # TODO: Make this loop more advanced
         try:
             while(1):
                 # Get current date and time (where the whistle is)
                 dt = datetime.now(self.tz)
-                # Check if a weekday
-                if self.Monday <= dt.weekday() <= self.Friday:
-                    # Check if during hours of 7AM to 5PM
-                    if self.morningHour <= dt.hour <= self.eveningHour:
-                        # Check if 5-after on any hour but 7AM
-                        if self.whistled == False and \
-                            dt.hour != self.morningHour and \
-                            dt.minute == self.classStartMinute:
-                            self.whistle()
-                        # Check if 5-till on any hour
-                        elif self.whistled == False and \
-                            dt.minute == self.classEndMinute:
-                            self.whistle()
-                        else:
-                            self.whistled = False
+                curTime = { "hour": dt.hour, "minute": dt.minute }
+                soundTimes = schedule[dt.weekday()]
+
+                if self.whistled == False and curTime in soundTimes
+                    self.scheduleWhistle()
+                else:
+                    self.whistled = False
         except Exception as e:
             print("Error during loop:", e)
 
 # ---
 
+# Custom file for authentication data not to be shared publicly
+twitterConfigFile   = "myConfig.json"
+# Custom file for holding all of the scheduled times the whistle should sound
+scheduleConfigFile  = "schedule.json"
+
 GT_Whistle = Whistler()
-GT_Whistle.setup()
-GT_Whistle.start()
+GT_Whistle.twitterSetup(GT_Whistle, twitterConfigFile)
+GT_Whistle.scheduleSetup(GT_Whistle, scheduleConfigFile)
+#GT_Whistle.start()
 
