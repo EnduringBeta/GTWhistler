141af24c60ce2ae14975d55c4a7f3d92db6e29e5
Boss1000 <boss1001@gmail.com>
2016-10-01 15:24:55 -0400

    Added football schedule API read and testing code. Can now download schedule!

--- Football.py
+++ Football.py
@@ -3,9 +3,10 @@
 
 # Example API code taken from devloper.fantasydata.com
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+import json
 import logging
 
-import Constants
+from Constants import *
 
 headers = None
 
@@ -16,27 +17,61 @@ def updateHeaders(subscriptionKey):
         'Ocp-Apim-Subscription-Key': subscriptionKey,
     }
 
-def accessFootballAPI(basePath, dataPath, params=urllib.parse.urlencode({})):
+def readFootballAPI(basePath, dataPath, params=urllib.parse.urlencode({})):
     if headers is None:
         logging.error("Error when calling football API: must define headers")
         return
     try:
-        conn = http.client.HTTPSConnection(Constants.APIserver)
+        conn = http.client.HTTPSConnection(APIserver)
         APIpath = basePath + dataPath + "?%s" % params
-        conn.request(Constants.APIget, APIpath, params, headers)
+        conn.request(APIget, APIpath, params, headers)
         response = conn.getresponse()
-        data = response.read()
-        logging.info(data)
+        dataStr = response.read().decode()
+        dataObj = json.loads(dataStr)
+
+        logging.info(dataStr)
         conn.close()
     except Exception as e:
-        logging.error("Failure when accessing footbal API: " + str(e))
+        logging.error("Failure when accessing football API: " + str(e))
+        return None
 
-#def checkForFootballSchedule():
-#    accessFootballAPI()
+    return dataObj
 
+def updateFootballSchedule(year, team):
+    fullSchedule = readFootballAPI(APIscorespath, APIschedule + year)
+    scheduleGT = []
 
-#def checkIfGameStarted():
-#    return
+    for game in fullSchedule:
+        if  game[APIfield_AwayTeam] == team or \
+            game[APIfield_HomeTeam] == team:
+            newGame = {}
+            newGame[APIfield_GameID]       = game[APIfield_GameID]
+            newGame[APIfield_DateTime]     = game[APIfield_DateTime]
+            newGame[APIfield_AwayTeam]     = game[APIfield_AwayTeam]
+            newGame[APIfield_HomeTeam]     = game[APIfield_HomeTeam]
+            newGame[APIfield_AwayTeamName] = game[APIfield_AwayTeamName]
+            newGame[APIfield_HomeTeamName] = game[APIfield_HomeTeamName]
+            scheduleGT.append(newGame)
+
+    # Write schedule to log and to file for later use
+    logging.info("Got football schedule:")
+    logging.info(json.dumps(scheduleGT, indent=4))
+    try:
+        with open(scheduleFootballFile, 'w') as outFile:
+            json.dump(scheduleGT, outFile, indent=4)
+    except Exception as e:
+        logging.error("Failure to write football schedule to file: " + str(e))
+        return None
+
+    return scheduleGT
+
+def readFootballSchedule():
+    try:
+        with open(scheduleFootballFile, 'r') as inFile:
+            return json.load(inFile)
+    except Exception as e:
+        logging.error("Failure to read football schedule from file: " + str(e))
+        return None
 
 # -----------------
 # --- EXECUTION ---
@@ -44,4 +79,5 @@ def accessFootballAPI(basePath, dataPath, params=urllib.parse.urlencode({})):
 
 # Testing
 updateHeaders("")
-accessFootballAPI(Constants.APIstatspath, Constants.APIboxscore + "1149")
+updateFootballSchedule("2016", APIdata_GTTeam)
+readFootballSchedule()
