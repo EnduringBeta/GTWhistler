3f33c40dc6ddba75ebc52bd64284a196fae32e5c
Ross Llewallyn <boss1001@gmail.com>
2020-06-25 11:03:37 -0400

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -5,7 +5,7 @@ from pytz import timezone
 from enum import Enum
 
 # Project constants
-versionNumber        = "v2.4.1"
+versionNumber        = "v2.4.2"
 
 # Note: Use this Boolean for testing
 debugDoNotTweet      = False # While in development, this Boolean prevents the
